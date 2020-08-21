625834ff3f5c11d4068aca4e5a9afb582e53f3a9
evengul <even.gultvedt@gmail.com>
2020-08-21 12:26:34 +0200

    Alle oppgavene er ferdige.

--- b.py
+++ b.py
@@ -1,39 +1,57 @@
 import torch
-from numpy import genfromtxt
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import (Enables 3D graphs)
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d as plt3d
 
-# Observed/training input and output
-all_x = torch.tensor(genfromtxt('day_length_weight.csv', delimiter=',', usecols=[0, 1])).double().t()
-all_y = torch.tensor(genfromtxt('day_length_weight.csv', delimiter=',', usecols=[2])).double().t()
+df = pd.read_csv('day_length_weight.csv')
 
+data = torch.tensor(df.values, dtype=torch.float)
+all_x = data[:, 0:2].t()
+all_y = data[:, 2].t()
+
+# can be changed to make x_test and y_test
 x_train = all_x
 y_train = all_y
+
+# inputs to our model
+n = 2
 
 
 class LinearRegressionModel:
     def __init__(self):
         # Model variables
-        self.W = torch.randn((1, 2), requires_grad=True)  # requires_grad enables calculation of gradients
+        self.W = torch.randn((1, n), requires_grad=True)  # requires_grad enables calculation of gradients
         self.b = torch.randn(1, requires_grad=True)
 
     # Predictor
     def f(self, x):
-        return x @ self.W.double() + self.b  # @ corresponds to matrix multiplication
+        return self.W.mm(x) + self.b
 
     # Uses Mean Squared Error
     def loss(self, x, y):
-        return torch.nn.functional.mse_loss(self.f(x), y)
+        return torch.nn.functional.mse_loss(self.f(x), y.reshape(1, 1000))
 
 
 model = LinearRegressionModel()
 
 # Optimize: adjust W and b to minimize loss using stochastic gradient descent
-optimizer = torch.optim.SGD([model.b, model.W], 0.0001)
-for epoch in range(10000):
+optimizer = torch.optim.SGD([model.W, model.b], 0.0000001)
+for epoch in range(75000):
     model.loss(x_train, y_train).backward()  # Compute loss gradients
     optimizer.step()  # Perform optimization by adjusting W and b,
     optimizer.zero_grad()  # Clear gradients for next step
 
 # Print model variables and loss
 print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
+
+fig = plt.figure().gca(projection='3d')
+
+fig.scatter(df['day'], df['length'], df['weight'], c='red')
+
+fig.scatter(x_train[0, :], x_train[1, :], zs=model.f(x_train).detach(), c='blue')
+
+fig.set_xlabel('Day')
+fig.set_ylabel('Length')
+fig.set_zlabel('Weight')
+
+plt.savefig('b')
--- c.py
+++ c.py
@@ -2,13 +2,12 @@ import torch
 import pandas as pd
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
+import math
 
-# Observed/training input and output
+df = pd.read_csv('day_head_circumference.csv')
 
-data = pd.read_csv('length_weight.csv')
-
-all_x = data["weight"].tolist()
-all_y = data["length"].tolist()
+all_x = df['day'].tolist()
+all_y = df['head circumference'].tolist()
 
 x_train = torch.tensor(all_x).reshape(-1, 1)
 y_train = torch.tensor(all_y).reshape(-1, 1)
@@ -17,12 +16,15 @@ y_train = torch.tensor(all_y).reshape(-1, 1)
 class LinearRegressionModel:
     def __init__(self):
         # Model variables
-        self.W = torch.tensor([[1.0]], requires_grad=True)  # requires_grad enables calculation of gradients
-        self.b = torch.tensor([[1.0]], requires_grad=True)
+        self.W = torch.randn((1, 1), requires_grad=True)  # requires_grad enables calculation of gradients
+        self.b = torch.randn((1, 1), requires_grad=True)
 
     # Predictor
     def f(self, x):
-        return x @ self.W + self.b  # @ corresponds to matrix multiplication
+        m = x @ self.W + self.b
+        s = torch.sigmoid(m)
+        y = 20.0 * s + 31.0
+        return y
 
     # Uses Mean Squared Error
     def loss(self, x, y):
@@ -32,8 +34,7 @@ class LinearRegressionModel:
 model = LinearRegressionModel()
 
 # Optimize: adjust W and b to minimize loss using stochastic gradient descent
-optimizer = torch.optim.SGD([model.W, model.b], 0.0001)
-# Extremely overfitting to the data, but the graph looks pretty
+optimizer = torch.optim.SGD([model.b, model.W], 0.000001)
 for epoch in range(75000):
     model.loss(x_train, y_train).backward()  # Compute loss gradients
     optimizer.step()  # Perform optimization by adjusting W and b,
@@ -46,7 +47,8 @@ print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_tra
 plt.plot(x_train, y_train, 'o', label='$(\\hat x^{(i)},\\hat y^{(i)})$')
 plt.xlabel('x')
 plt.ylabel('y')
-x = torch.tensor([[torch.min(x_train)], [torch.max(x_train)]])  # x = [[1], [6]]]
-plt.plot(x, model.f(x).detach(), label='$y = f(x) = xW+b$')
+x = torch.tensor([[torch.min(x_train)], [torch.mean(x_train)], [torch.max(x_train)]])
+
+plt.plot(x, model.f(x).detach(), label='$y = f(x) = 20*sigmoid(xW+b)+31$')
 plt.legend()
-plt.savefig(fname='a')
+plt.savefig(fname='c')
